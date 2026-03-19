b176ecde84dfa7364e1b1d509346c1e93f263406
blindsector <danislavslavev4@gmail.com>
2026-03-19 22:33:46 +0200

    sound 3

--- client/js/app.js
+++ client/js/app.js
@@ -8,22 +8,12 @@ function startApp() {
     }
 
     bootstrapSession();
-}
-
-function startApp() {
-    bindEvents();
-
-    state.onlineVisibilityEnabled = loadOnlineVisibilitySetting();
-
-    if (typeof onlineVisibilityToggle !== "undefined" && onlineVisibilityToggle) {
-        onlineVisibilityToggle.checked = state.onlineVisibilityEnabled;
-    }
-
-    bootstrapSession();
 
     setTimeout(() => {
-        if (state.user) {
+        if (state.user && typeof feedback !== "undefined") {
             feedback.init();
         }
     }, 500);
 }
+
+startApp();
